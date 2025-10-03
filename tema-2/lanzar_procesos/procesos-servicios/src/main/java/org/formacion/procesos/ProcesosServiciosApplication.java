003fd9687fdaba94000ed1840d51d9c230eda6c7
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2025-10-03 17:59:46 +0100

    se sube la terea completada y comentada

---
+++ tema-2/lanzar_procesos/procesos-servicios/src/main/java/org/formacion/procesos/ProcesosServiciosApplication.java
@@ -0,0 +1,33 @@
+package org.formacion.procesos;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author alejandrosalazargonzalez
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class ProcesosServiciosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcesosServiciosApplication.class, args);
+    }
+
+    /**
+     * ejecuta los procesos y avisa cuando ha terminado
+     */
+    @Bean
+    CommandLineRunner demo(Procesos procesos) {
+        return args -> {
+            System.out.println("Iniciando proceso al arrancar la aplicación...");
+
+            procesos.ejecutar();
+            procesos.procesosEnFichero();
+            procesos.contarLineas();
+            System.out.println("Proceso finalizado.");
+        };
+    }
+}
